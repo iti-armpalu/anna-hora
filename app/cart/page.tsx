80c01f233aac63571ee671b096a18a1d69328b79
Iti Armpalu <iti.armpalu@gmail.com>
2025-12-12 14:58:14 +0200

    anna hora v40.1

--- app/cart/page.tsx
+++ app/cart/page.tsx
@@ -1,13 +1,6 @@
-// app/cart/page.tsx
-import type { Metadata } from "next"
-import { formatTitle, SEO } from "@/config/seo"
 import CartPageClient from "./cart-page-client"
 import { getFreeShippingThreshold } from "@/lib/geo/get-free-shipping-treshold";
 
-export const metadata: Metadata = {
-  title: formatTitle("Your Bag"),
-  description: SEO.description,
-}
 export default async function CartPage() {
   const { threshold } = await getFreeShippingThreshold();
 
